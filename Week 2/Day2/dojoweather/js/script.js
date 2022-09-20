316b9c7887d75b9593243064a65387a16d13cbe6
Radyn <radyn.mort22@gmail.com>
2022-09-20 12:07:43 -0700

    dojo weather 2/3 javascript done

---
+++ Week 2/Day2/dojoweather/js/script.js
@@ -0,0 +1,14 @@
+function loading(element){
+    alert("Loading weather report...")
+}
+
+function tempType(){
+    let min = document.getElementsByClassName(".min")
+    let max = document.querySelector(".max") 
+    console.log(min)
+    //call all mins and max then edit there content by mutiplying a var
+}
+
+function acceptCookies(){
+    document.querySelector(".cookieJar").remove();
+}
