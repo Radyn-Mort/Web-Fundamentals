ffb861f3c17a8d975e231feb43141700aa3cd359
Radyn <radyn.mort22@gmail.com>
2022-09-21 07:47:18 -0700

    dojoweather finished

--- Week 2/Day2/dojoweather/js/script.js
+++ Week 2/Day2/dojoweather/js/script.js
@@ -2,18 +2,22 @@ function loading(element){
     alert("Loading weather report...")
 }
 
-function tempType(element){
-    let allTemps = document.querySelectorAll('.min,.max')
-    if(element.value==='fa'){
-        for(let i =0; i<allTemps;i++){
-            let currentTemp = Math.round(Number(allTemps[i].textContent)* 1.8+32)
-            console.log(currentTemp)
+function changeTemperature( element ){
+    let allDegrees = document.querySelectorAll( ".max, .min" );
+    
+    for( let i = 0; i < allDegrees.length; i ++ ){
+        let currentNum;
+        console.log(element.value)
+        if( element.value === "far" ){
+            
+            currentNum = Math.round( Number(allDegrees[i].textContent) * 1.8 + 32 );
+            console.log(currentNum)
         }
+        else{
+            currentNum = Math.round( (Number(allDegrees[i].textContent) - 32) * .5556 );
+        }
+        allDegrees[i].textContent = currentNum;
     }
-    if(element.value==='ce'){
-
-    }
-    allTemps[i].textContent = currentTemp;
 }
 
 function acceptCookies(){
